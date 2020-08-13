7fb74ac2c6df1cbbee53efa8dabec1d435ba3711
B1gMeech <correa3hunnit@gmail.com>
2020-08-13 04:27:42 +0200

    first commit

---
+++ precache-manifest.c5bdd53e1ed7e592e09e9e1de7898167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a401c7d7823c7454c5794a0ecf1e5e56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6cbbde8c3401f011710",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "f1a985b9b8d0b10da145",
+    "url": "/static/css/main.ce67f212.chunk.css"
+  },
+  {
+    "revision": "a6cbbde8c3401f011710",
+    "url": "/static/js/2.3363d6e2.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/static/js/2.3363d6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a985b9b8d0b10da145",
+    "url": "/static/js/main.9b405233.chunk.js"
+  },
+  {
+    "revision": "7ccab73da36156780c6d",
+    "url": "/static/js/runtime-main.77dc2048.js"
+  }
+]);
